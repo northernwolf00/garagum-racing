c5615c8acb8b168adc48ece87be3ab309660bdde
northernwolf00 <guwanchaldurdyyew01@gmail.com>
2026-09-05 21:07:56 +0500

    v1.0.2+3: FlutterFragmentActivity for RevenueCat Paywalls + version bump

--- android/app/src/main/kotlin/com/googadev/garagum_racing/MainActivity.kt
+++ android/app/src/main/kotlin/com/googadev/garagum_racing/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.googadev.garagum_racing
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
